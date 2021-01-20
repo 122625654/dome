095b657a93bdcd3c7ed5b8190f9609b15a28de23
huang <122625654@qq.com>
2021-01-20 17:36:50 +0800

    首页添加视频

---
+++ src/components/common/Buttom.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="buttom">
+      element <span>@</span> 2020 vue
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+  .buttom {
+    width: 100%;
+    margin-top: 20px;
+    text-align: center;
+  }
+
+  .buttom span {
+    color:#409EFF;
+  }
+</style>
